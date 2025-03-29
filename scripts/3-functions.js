56b8c4355ff3225c4c43d06f913e2bb096e5b218
fvalverdeu <fvalverdeu@gmail.com>
2025-03-28 23:52:51 -0500

    add 3-functions

---
+++ scripts/3-functions.js
@@ -0,0 +1,93 @@
+const nombre = document.getElementById('nombre');
+const apellido = document.getElementById('apellido');
+const correo = document.getElementById('correo');
+
+// const objeto = {
+//     propiedad1: 1,
+//     otraFuncion() {
+//         miFuncion();
+//         funcion();
+//         console.log(this);    
+//     }
+// }
+
+// function funcion() {
+//     console.log(this);
+// }
+// const miFuncion = () => {console.log(this)}
+
+// // miFuncion();
+// objeto.otraFuncion();
+
+// if (false) {
+//     variable;
+// } else {
+//     let variable;
+// }
+
+// let operacion = {
+//     nombre: 'contar-funcion',
+//     contar(params) {
+//         console.log(this);
+//     }
+// }
+
+// operacion.contar(0);
+
+// function contar(params) {
+//     console.log(this);
+//     // var terceraVariable;
+//     // console.log(this.variable);
+//     // let nan = NaN;
+//     // console.log(typeof(nan));
+//     // console.log(typeof(this.variable));
+//     // console.log(window.variable)
+//     // this.variable++;
+
+//     // setTimeout(() => {
+//     //     window.close();
+//     // }, 5000);
+// }
+// contar(0);
+
+// let variable;
+// variable = 1;
+
+// var otraVariable;
+
+/*
+(function name(params, repeticiones) {
+    // alert('ALERT ' + params)
+    let i = 0;
+    while (i < repeticiones) {
+        console.log('hola ' + params);
+        i++;
+    }
+})('Alexander', 5)
+*/
+//--------------------------------------------------------------
+
+
+// document.getElementById("boton").addEventListener("click", saludar('Alexander'));
+
+// document.getElementById('boton').addEventListener('click',function() {
+//     saludar('Alexander')
+// });
+
+
+// document.getElementById("cambio").addEventListener("click", () => {
+//     document.getElementById("boton").removeEventListener();
+// });
+
+
+// document.getElementById("boton").addEventListener("click", () => {
+//     saludar("¡Hola desde addEventListener!");
+// });
+
+// function saludar(nombre) {
+//     console.log('Holaaa ' + nombre);
+// }
+
+// function despedir() {
+//     console.log('chauuuu');
+// }
